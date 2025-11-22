d2ec1d26c26426ef3f816064ac329b470d47c1a5
Gurmessa <gurmessa@prompt.com>
2025-11-22 12:27:03 +0300

    overall metric

---
+++ backend/app/services/metrics/constants.py
@@ -0,0 +1,18 @@
+STRUCTURAL_INDICATORS = [
+    "first",
+    "second",
+    "third",
+    "finally",
+    "in conclusion",
+    "however",
+    "moreover",
+    "additionally",
+    "therefore",
+    "thus",
+]
+CONCLUSION_WORDS = [
+    "in summary",
+    "in conclusion",
+    "to summarize",
+    "overall",
+]
