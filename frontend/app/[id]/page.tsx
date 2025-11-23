4f1fb80324c0aa4a654df2a1da4e250d231a87ad
Gurmessa <gurmessa@prompt.com>
2025-11-24 00:44:25 +0300

    SE0

--- frontend/app/[id]/page.tsx
+++ frontend/app/[id]/page.tsx
@@ -6,6 +6,20 @@ interface PageProps {
   params: { id: string };
 }
 
+export async function generateMetadata({ params }: PageProps) {
+  const { id } =  await params;
+  const experiment: Experiment = await getExperimentDetail(id);
+
+  return {
+    title: experiment.name || `Experiment ${params.id}`,
+    description: experiment.user_prompt,
+    openGraph: {
+      title: experiment.name || `Experiment ${params.id}`,
+      description: experiment.user_prompt,
+    },
+  };
+}
+
 export default async function ExperimentDetail({ params }: PageProps) {
   const { id } =  await params;
   const experiment: Experiment = await getExperimentDetail(id);
