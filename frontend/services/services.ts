91abcedd182b1c4e90fdbed4758a339eaf017425
Gurmessa <gurmessa@prompt.com>
2025-11-24 20:33:22 +0300

    update services

--- frontend/services/services.ts
+++ frontend/services/services.ts
@@ -4,24 +4,24 @@ import { api } from "@/lib/api";
 import { Experiment, ExperimentDetail, ExperimentCreate } from "@/types/types";
 
 export async function listExperiments(): Promise<Experiment[]> {
-  const { data } = await api.get("/experiments");
+  const { data } = await api.get("/experiments/");
   return data;
 }
 
 export async function getExperimentDetail(id: string): Promise<ExperimentDetail> {
-  const { data } = await api.get(`/experiments/${id}`);
+  const { data } = await api.get(`/experiments/${id}/`);
   return data;
 }
 
 export async function createExperiment(experiment: ExperimentCreate): Promise<ExperimentDetail> {
-  const { data } = await api.post("/experiments", experiment);
+  const { data } = await api.post("/experiments/", experiment);
   return data;
 }
 
 
 export const exportExperimentCsv = async (experimentId: number) => {
   const response = await api.get(
-    `/experiments/${experimentId}/export/csv`,
+    `/experiments/${experimentId}/export/csv/`,
     {
       responseType: "blob", // important for file downloads
     }
